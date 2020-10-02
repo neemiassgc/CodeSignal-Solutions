9c456376c56fbab2190efbd0fdb9fcac29ad4faf
Neemias Santos <neemiassantos@pm.me>
2020-10-02 15:41:38 -0300

    Update AllLongestStrings.java

--- Arcade/Intro/Java/AllLongestStrings.java
+++ Arcade/Intro/Java/AllLongestStrings.java
@@ -3,15 +3,13 @@ import java.util.Arrays;
 public class AllLongestStrings {
 
 	String[] allLongestStrings(String[] inputArray) {
-	    int max = Arrays
-	        .stream(inputArray)
-	        .mapToInt(n -> n.length())
-	        .max()
-	        .getAsInt();
-	    Object[] obj = Arrays
-	        .stream(inputArray)
-	        .filter(s -> s.length() == max)
-	        .toArray();
-	    return Arrays.asList(obj).toArray(new String[obj.length]);
+	    int max = Stream.<String>of(inputArray)
+		.mapToInt(n -> n.length())
+		.max()
+		.getAsInt();
+		
+	    return Stream.<String>of(inputArray)
+		.filter(e -> e.length() == max)
+		.toArray(String[]::new);
 	}
 }
